af2680c4c3b0688b41f73a4b122a1fb5ec051ea1
Anthony <Anthony@dunford.uk.com>
2018-01-12 14:11:24 +0000

    initial changes

--- MainCode.py
+++ MainCode.py
@@ -5,8 +5,8 @@ pygame.init()
 clock = pygame.time.Clock()
 #test comment
 
-display_width = 500
-display_height = 500
+display_width = 450
+display_height = 450
 game_display = pygame.display.set_mode((display_width,display_height))
 pygame.display.set_caption('Ulitmate Naughts and Crosses')
 
@@ -19,8 +19,9 @@ blue   = (0,0,255)
 cyan   = (0,255,255)
 yellow = (255,255,0)
 
+counter = 0
 
-true = True
-
-while true:
-    clock.tick(60)
+while counter < 20:
+    
+    clock.tick(10)
+    counter += 1
